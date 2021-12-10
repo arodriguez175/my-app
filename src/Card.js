dd5d121e23c025fb1ed76011ae0964906193e600
Anna Kurylo <akurylo@redhat.com>
2021-12-10 19:50:42 +0100

    render a different icon depending on acitivity type

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { ReactComponent as Icon } from "./images/icon-work.svg";
+import { ReactComponent as WorkIcon } from "./images/icon-work.svg";
+import { ReactComponent as PlayIcon } from "./images/icon-play.svg";
 import "./Card.css";
 
+// depending on activity type, render a different icon
+const icons = {
+  work: <WorkIcon/>,
+  play: <PlayIcon/>,
+}
 class Card extends React.Component {
   render() {
     const activityTitle = this.props.activityTitle || "";
     return (
       <article className="background-card" id={activityTitle.toLowerCase()}>
-        <Icon />
+        {/* get an icon for the activity */}
+        {icons[activityTitle.toLowerCase()]}
         <div className="card-body">
           <p className="activity-titles">{activityTitle}</p>
           <p className="time">{this.props.time}hrs</p>
