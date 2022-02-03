3e71c22ca1600cb3495be2bd39ff124d37988fcc
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-02-03 16:57:52 -0500

    Removed commented out code..

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 export function formatToCapitalCase(word) {
-  // 1. take the first character of a word ("work" -> "w", "play" -> "p", "selfcare" -> "s")
+  // 1. takes the first character of a word ("work" -> "w", "play" -> "p", "selfcare" -> "s")
   let firstLetter = word.charAt(0);
-  // 2. make this character uppercase (example: "w" -> "W")
+  // 2. makes this character uppercase (example: "w" -> "W")
   firstLetter = firstLetter.toUpperCase();
-  // 3. put your capitalized character as a first character of your word ("work" -> "Work")
-  // 4. return the capitalized word
+  // 3. puts capitalized character as a first character of the word ("work" -> "Work")
+  // 4. returns the capitalized word
   return firstLetter + word.substr(1);
 }
