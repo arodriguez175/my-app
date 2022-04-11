079b1d13ce2789e2a8b0204bd6e61ee93a3cf4bc
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-04-11 14:36:56 -0400

    Rendered activity cards using prop.

--- src/App.js
+++ src/App.js
@@ -3,106 +3,6 @@ import { connect } from "react-redux";
 import Card from "./Card";
 import Profile from "./Profile";
 import UserInput from "./UserInput";
-// import { activityStats } from "./store";
-
-let timeSpentData = {
-  daily: [
-    {
-      activityType: "work",
-      currentHours: 5,
-      previousHours: 7,
-    },
-    {
-      activityType: "play",
-      currentHours: 1,
-      previousHours: 2,
-    },
-    {
-      activityType: "study",
-      currentHours: 0,
-      previousHours: 1,
-    },
-    {
-      activityType: "exercise",
-      currentHours: 1,
-      previousHours: 1,
-    },
-    {
-      activityType: "social",
-      currentHours: 1,
-      previousHours: 3,
-    },
-    {
-      activityType: "selfcare",
-      currentHours: 0,
-      previousHours: 1,
-    },
-  ],
-  weekly: [
-    {
-      activityType: "work",
-      currentHours: 32,
-      previousHours: 36,
-    },
-    {
-      activityType: "play",
-      currentHours: 10,
-      previousHours: 8,
-    },
-    {
-      activityType: "study",
-      currentHours: 4,
-      previousHours: 7,
-    },
-    {
-      activityType: "exercise",
-      currentHours: 4,
-      previousHours: 5,
-    },
-    {
-      activityType: "social",
-      currentHours: 5,
-      previousHours: 10,
-    },
-    {
-      activityType: "selfcare",
-      currentHours: 2,
-      previousHours: 2,
-    },
-  ],
-  monthly: [
-    {
-      activityType: "work",
-      currentHours: 103,
-      previousHours: 128,
-    },
-    {
-      activityType: "play",
-      currentHours: 23,
-      previousHours: 29,
-    },
-    {
-      activityType: "study",
-      currentHours: 13,
-      previousHours: 19,
-    },
-    {
-      activityType: "exercise",
-      currentHours: 11,
-      previousHours: 18,
-    },
-    {
-      activityType: "social",
-      currentHours: 21,
-      previousHours: 23,
-    },
-    {
-      activityType: "selfcare",
-      currentHours: 7,
-      previousHours: 11,
-    },
-  ],
-};
 
 class App extends React.Component {
   constructor(props) {
@@ -165,7 +65,7 @@ class App extends React.Component {
           </div>
 
           {/* Replaced timeSpentData with activityStats */}
-          {timeSpentData[this.state.currentView].map((item) => {
+          {this.props.activityStats[this.state.currentView].map((item) => {
             return (
               <div class="grid-item">
                 {/* Card component with props for dynamic changes. */}
